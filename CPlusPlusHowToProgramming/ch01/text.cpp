585f7100cc9e4970f369ec342a95f9b58c2f3e1a
mario-aj <mariojorge1997@mail.ru>
2023-02-14 10:48:36 +0100

    config github

---
+++ CPlusPlusHowToProgramming/ch01/text.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string name;
+
+	cout << "Morning, type your name please: ";
+	cin >> name;
+	
+	cout << "Hello, " << name << endl;	
+  
+	return 0;
+}
