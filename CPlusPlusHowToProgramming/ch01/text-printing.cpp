3352b1b753553c397ec8db25216cd225c6c8f85e
mario-aj <mariojorge1997@mail.ru>
2023-02-14 12:08:01 +0100

    print multiple format with c++

--- CPlusPlusHowToProgramming/ch01/text-printing.cpp
+++ CPlusPlusHowToProgramming/ch01/text-printing.cpp
@@ -2,7 +2,14 @@
 #include <iostream> // enables program to output data to the screen
 
 int main() {
-	std::cout << "Welcome to C++!\n";
+	std:: cout << "Welcome to C++\n";
+
+	// print a line of text in multiple statements
+	std::cout << "Welcome ";
+	std::cout << "to C++!\n";
 	
+	// printing multiple lines of text with a single statement
+	std::cout << "Welcome\nto\nC++!\n"; 
+
 	return 0;
 }
